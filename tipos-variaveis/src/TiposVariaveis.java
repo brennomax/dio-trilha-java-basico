a7465829543d3e51caadfbd345a330170c35804e
Breno Max <brenomaxufmg@gmail.com>
2024-05-08 00:49:59 -0300

    implementação dos códigos no arquivo TiposVariaveis.java para representação de tipos e variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,6 +2,13 @@
 public class TiposVariaveis {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String nome = "Breno Max"; // classe que representa textos sempre entre aspas duplas
+        char sexo = 'M'; // char armazena caracteres e sempre entre aspas simples
+        boolean valor = true; // valores lógicos no qual true = 1 e false = 0
+        double salarioMinimo = 2500.34;
+        final double VALOR_DE_PI = 3.14; // final representa um valor constante, ou seja, que nunca poderá mudar ao longo do código
+        float salarioMinimoFloat = 2500.34F; // float necessita de f ou F ao final do valor se não será considerado um tipo double
+        int codigo = 34; // se começar com 0 (zero) deve ser string, pois o tipo int não o armazena 
+        long cpf = 12303546102L; // long necessita de l ou L ao final do valor se não será considerado um tipo int
     }
 }

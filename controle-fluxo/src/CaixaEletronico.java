1ca46585ea129c8df5739638f76beaa54f6371be
Breno Max <brenomaxufmg@gmail.com>
2024-05-09 10:19:27 -0300

    update: incremento de um sistema de validação de saldo

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -1,12 +1,18 @@
+import java.util.Scanner;
+
 public class CaixaEletronico {
     public static void main(String[] args) throws Exception {
         double saldo = 2500;
-        double valorSolicitado = 2700;
+        double valorSolicitado;
+        Scanner scanner = new Scanner (System.in);
+
+        valorSolicitado = scanner.nextDouble();
 
         if (valorSolicitado < saldo) {
             saldo = saldo - valorSolicitado;
-        }
-
-        System.out.println("Você sacou " + valorSolicitado + " e o seu novo saldo é: " + saldo);
+            System.out.println("Você sacou " + valorSolicitado + " e o seu novo saldo é: " + saldo);
+        }else{
+            System.out.println("Slado insuficiente.");
+        }        
     }
 }
